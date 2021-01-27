feb58bda0684e2af851340775795a5d8f9787541
1020115193@qq.com <HUpeng950202>
2021-01-27 17:31:43 +0800

    修改了pom文件相关的代码

--- renovation-server-dashboard/src/main/java/com/renovation/dashboard/RenovationServerDashboardApplication.java
+++ renovation-server-dashboard/src/main/java/com/renovation/dashboard/RenovationServerDashboardApplication.java
@@ -1,12 +1,14 @@
 package com.renovation.dashboard;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@SpringBootApplication
+@SpringCloudApplication //相当于这三个注解@SpringBootApplication @EnableDiscoveryClient @EnableCircuitBreaker
 @EnableHystrixDashboard
 public class RenovationServerDashboardApplication {
+
+    // dashboard访问地址： http://127.0.0.1:8080/hystrix
 
     public static void main(String[] args) {
         SpringApplication.run(RenovationServerDashboardApplication.class, args);
